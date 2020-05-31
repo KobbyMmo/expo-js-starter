0d2a64f771e6f465fc0da04dd92e3c36761648cf
Obed Amoasi <obedamoasi@gmail.com>
2020-05-31 09:20:49 +0000

    configure redux and navigation

--- App.js
+++ App.js
@@ -1,19 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Platform,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  Vibration,
+} from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './Store';
+import { loadState } from './Store/Storage';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import { AppLoading, Icon, SecureStore } from 'expo';
+import AppNavigator from './navigation/AppNavigator';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      persistedState: {},
+    };
+  }
+
+  async componentDidMount() {
+    const _persistedState = await loadState();
+    this.setState({ persistedState: _persistedState });
+  }
+
+  render() {
+    if (Object.keys(this.state.persistedState).length > 0) {
+      return (
+        <Provider store={store(this.state.persistedState)}>
+          <View style={styles.container}>
+            {/* {Platform.OS === 'ios' ? (
+            ) : (
+              <StatusBar hidden={true} />
+            )} */}
+            <StatusBar barStyle='default' />
+
+            <AppNavigator />
+          </View>
+        </Provider>
+      );
+    }
+    return (
+      <View>
+        <Text>Loading</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
