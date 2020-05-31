4b057d07aca5213c6e22867d07a61a4ab49eb148
Obed Amoasi <obedamoasi@gmail.com>
2020-05-31 09:09:44 +0000

    add account action types

---
+++ Store/ActionType.js
@@ -0,0 +1,11 @@
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_UP = 'SIGN_UP';
+export const LOGOUT = 'LOGOUT';
+export const ERROR = 'ERROR';
+
+export default {
+  SIGN_IN,
+  ERROR,
+  SIGN_UP,
+  LOGOUT,
+};
